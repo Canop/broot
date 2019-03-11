Canop/broot

commit 3bf432fd6876f60c4ffdc47297fe960250145d83

Let's admit skin conf code is less ugly (not yet likable but way less ugly)

src/skin_conf.rs
@@ -4,8 +4,6 @@ use regex::Regex;
 /// defining foreground and background colors into
 /// a string with TTY colors
 ///
-/// The code is ugly. Really. I know.
-/// I found no clean way to deal with termion colors.
 use std::result::Result;
 use termion::color::*;
 
@@ -69,7 +67,7 @@ define_color_from_name! {
     Red,
     Reset,
     White,
-    Yellow
+    Yellow,
 }
 
 pub fn parse_config_entry(key: &str, value: &str) -> Result<String, ConfError>{

commit 7321035628acfcab16bfc70857de4bf8a5af597b

Fix compilation
3bf432fd6876f60c4ffdc47297fe960250145d83 broke compilation

src/skin_conf.rs
@@ -35,7 +35,7 @@ impl std::fmt::Display for TypedColor {
 }
 
 macro_rules! define_color_from_name {
-    ( $($name:ident),* ) => {
+    ( $($name:ident,)* ) => {
         fn color_from_name(raw: &str) -> Result<Box<Color>, InvalidSkinError> {
             if raw.eq_ignore_ascii_case("none") {
                 Ok(Box::new(Reset))