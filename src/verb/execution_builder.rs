2f4027ce1741098147b70e5de4d853d3ae8a86be
Canop <cano.petrole@gmail.com>
2021-04-17 10:38:59 +0200

    fix test unit on changed AP

--- src/verb/execution_builder.rs
+++ src/verb/execution_builder.rs
@@ -170,7 +170,7 @@ mod execution_builder_test {
             stype: SelectionType::File,
             is_exe: false,
         };
-        let mut builder = ExecutionStringBuilder::from_selection(sel);
+        let mut builder = ExecutionStringBuilder::from_selection(Some(sel));
         let mut map = AHashMap::default();
         for (k, v) in replacements {
             map.insert(k.to_owned(), v.to_owned());
