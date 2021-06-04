686e6ee62dd647c0c5d5d99947d6b8e3299629f3
Canop <cano.petrole@gmail.com>
2021-06-04 15:49:26 +0200

    fix broken compilation when feature "client-server" is enabled

--- src/app/app.rs
+++ src/app/app.rs
@@ -510,8 +510,10 @@ impl App {
         self.update_preview(con);
 
         #[cfg(feature="client-server")]
-        if let Ok(mut root) = self.root.lock() { // when does this not work ?
-            *root = self.state().selected_path().to_path_buf();
+        if let Ok(mut root) = self.root.lock() {
+            if let Some(path) = self.state().selected_path() {
+                *root = path.to_path_buf();
+            }
         }
 
         Ok(())
