d8c943716fb02185c66d8d821cabf872e2ef67f6
Canop <cano.petrole@gmail.com>
2022-01-22 08:00:29 +0100

    dump in log the launch command
    This may (or not) help to diagnose #483

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use cli_log::*;
 
 fn main() {
     init_cli_log!();
+    debug!("env::args(): {:#?}", std::env::args().collect::<Vec<String>>());
     match broot::cli::run() {
         Ok(Some(launchable)) => {
             debug!("launching {:#?}", launchable);
