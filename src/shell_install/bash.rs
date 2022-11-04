2b1dddaace34f41d2f88d02c483aef3eb53e3742
Jakub Kaczor <57760986+jakubkaczor@users.noreply.github.com>
2022-11-04 21:37:52 +0100

    Prevent `br` from using `rm` alias or function in Bash (#629)
    Co-authored-by: Jakub Kaczor <jakub@jakubkaczor.com>

--- src/shell_install/bash.rs
+++ src/shell_install/bash.rs
@@ -43,11 +43,11 @@ function br {
     cmd_file=$(mktemp)
     if broot --outcmd "$cmd_file" "$@"; then
         cmd=$(<"$cmd_file")
-        rm -f "$cmd_file"
+        command rm -f "$cmd_file"
         eval "$cmd"
     else
         code=$?
-        rm -f "$cmd_file"
+        command rm -f "$cmd_file"
         return "$code"
     fi
 }
