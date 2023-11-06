4856d360c83ca0ac6a341afa577ec3e875de113c
Karl-Johan Karlsson <creideiki@ferretporn.se>
2023-11-06 14:24:04 +0100

    Switch dependency from "users" to "uzers"
    "users" is unmaintained (RUSTSEC-2023-0040) and has a listed
    vulnerability (RUSTSEC-2023-0059). Switch to the maintained and
    updated fork "uzers", and upgrade to 0.11.3 that has the fix for
    RUSTSEC-2023-0059.

--- src/permissions/permissions_unix.rs
+++ src/permissions/permissions_unix.rs
@@ -16,7 +16,7 @@ pub fn user_name(uid: u32) -> String {
     let name = users_cache
         .entry(uid)
         .or_insert_with(|| {
-            users::get_user_by_uid(uid).map_or_else(
+            uzers::get_user_by_uid(uid).map_or_else(
                 || "????".to_string(),
                 |u| u.name().to_string_lossy().to_string(),
             )
@@ -32,7 +32,7 @@ pub fn group_name(gid: u32) -> String {
     let name = groups_cache
         .entry(gid)
         .or_insert_with(|| {
-            users::get_group_by_gid(gid).map_or_else(
+            uzers::get_group_by_gid(gid).map_or_else(
                 || "????".to_string(),
                 |u| u.name().to_string_lossy().to_string(),
             )
