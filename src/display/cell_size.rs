26d6110ecfa851311e60f9d5884056615cfa9ac8
Denys Séguret <cano.petrole@gmail.com>
2024-07-05 16:05:13 +0200

    Issue 885 (#897)
    * remove name and subpath from bline
    
    * remove fnv dependency
    
    * fix brocken ioctl call in release mode
    
    Fix #885
    
    * better fix of the ioctl call being removed in release

--- src/display/cell_size.rs
+++ src/display/cell_size.rs
@@ -23,20 +23,21 @@ pub fn cell_size_in_pixels() -> std::io::Result<(u32, u32)> {
         ws_xpixel: c_ushort, /* horizontal size, pixels */
         ws_ypixel: c_ushort, /* vertical size, pixels */
     }
-    let w = winsize {
+    let mut w = winsize {
         ws_row: 0,
         ws_col: 0,
         ws_xpixel: 0,
         ws_ypixel: 0,
     };
     #[allow(clippy::useless_conversion)]
-    let r = unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ.into(), &w) };
+    let r = unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ.into(), &mut w) };
     if r == 0 && w.ws_xpixel > w.ws_col && w.ws_ypixel > w.ws_row {
         Ok((
             (w.ws_xpixel / w.ws_col) as u32,
             (w.ws_ypixel / w.ws_row) as u32,
         ))
     } else {
+        warn!("failed to fetch cell dimension with ioctl");
         Err(io::Error::new(
             io::ErrorKind::Other,
             "failed to fetch terminal dimension with ioctl",
