44091b4a04d9cce67e2a6c080bdc9ae007181fef
Denys Séguret <cano.petrole@gmail.com>
2024-07-13 08:10:15 +0200

    Preview transformers (#898)
    This allows defining file transformations that are applied to files before preview.
    
    The default conf has 2 examples: previewing PDF and beautifying JSON: 
    
    ```Hjson
    preview_transformers: [
         # Use mutool to render any PDF file as an image
         # In this example we use placeholders for the input and output files
         {
             input_extensions: [ "pdf" ] // case doesn't matter
             output_extension: png
             mode: image
             command: [ "mutool", "draw", "-o", "{output-path}", "{input-path}" ]
         }
    
         # Use jq to beautify JSON
         # In this example, the command refers to neither the input nor the output,
         # so broot pipes them to the stdin and stdout of the jq process
         {
             input_extensions: [ "json" ] 
             output_extension: json
             mode: text
             command: [ "jq" ]
         }
    ]
    ```
    
    PDF preview:
    
    ![image](https://github.com/Canop/broot/assets/617006/4c140060-573f-4368-b98b-61443f2f56c9)
    
    JSON beautification (the original file has no space nor new line):
    
    ![image](https://github.com/Canop/broot/assets/617006/9363bd0e-0ad4-43cc-956d-6d807e40d11c)
    
    Fix #494

--- src/preview/mod.rs
+++ src/preview/mod.rs
@@ -1,23 +1,26 @@
 mod dir_view;
 mod preview;
+mod preview_transformer;
 mod preview_state;
 mod zero_len_file_view;
 
 pub use {
     dir_view::DirView,
     preview::Preview,
+    preview_transformer::*,
     preview_state::PreviewState,
     zero_len_file_view::ZeroLenFileView,
 };
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, serde::Deserialize)]
+#[serde(rename_all = "snake_case")]
 pub enum PreviewMode {
 
     /// image
     Image,
 
     /// show the content as text, with syntax coloring if
-    /// it makes sens. Fails if the file isn't in UTF8
+    /// it makes sense. Fails if the file isn't in UTF8
     Text,
 
     /// show the content of the file as hex
