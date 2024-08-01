7927fec14fe54fba81103d6c696bea7f30aca0de
Jaakko Paju <jaakko.paju@wolt.com>
2024-08-01 15:32:01 +0300

    Update nerdfont and vscode icons (#906)

--- resources/icons/vscode/data/file_name_to_icon_name_map.rs
+++ resources/icons/vscode/data/file_name_to_icon_name_map.rs
@@ -1,5 +1,13 @@
 [
-	( "license"     , "file_type_license"           ),
-	( "todo"        , "file_type_todo"              ),
-	( "readme"      , "file_type_text"              ),
+    ( ".scalafix.conf"             , "file_type_config" ),
+    ( ".scalafmt.conf"             , "file_type_config" ),
+    ( "build.properties"           , "file_type_config" ),
+    ( "eslint.config.cjs"          , "file_type_eslint" ),
+    ( "eslint.config.js"           , "file_type_eslint" ),
+    ( "eslint.config.mjs"          , "file_type_eslint" ),
+    ( "license"                    , "file_type_license"),
+    ( "package-lock.json"          , "file_type_npm"    ),
+    ( "package.json"               , "file_type_npm"    ),
+    ( "readme"                     , "file_type_text"   ),
+    ( "todo"                       , "file_type_todo"   ),
 ]
