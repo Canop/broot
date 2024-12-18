22818d1d01e9efd422a80ca79047849c66c317fa
Kid <44045911+kidonng@users.noreply.github.com>
2024-12-18 22:21:05 +0800

    Remove eval usage in fish launcher

--- src/shell_install/fish.rs
+++ src/shell_install/fish.rs
@@ -35,9 +35,8 @@ const FISH_FUNC: &str = r"
 function br --wraps=broot
     set -l cmd_file (mktemp)
     if broot --outcmd $cmd_file $argv
-        read --local --null cmd < $cmd_file
+        source $cmd_file
         rm -f $cmd_file
-        eval $cmd
     else
         set -l code $status
         rm -f $cmd_file
