805c943964ca8f40e55e37f5f1f6867d326f668f
Canop <cano.petrole@gmail.com>
2025-01-03 08:06:23 +0100

    update syntect-no-panic

--- src/syntactic/syntaxer.rs
+++ src/syntactic/syntaxer.rs
@@ -5,7 +5,10 @@ use {
     once_cell::sync::Lazy,
     std::path::Path,
     syntect::{
-        easy::HighlightLines,
+        easy::{
+            HighlightLines,
+            HighlightOptions,
+        },
         parsing::SyntaxSet,
         highlighting::{Theme, ThemeSet},
     },
@@ -51,7 +54,10 @@ impl Syntaxer {
                 let theme = con.syntax_theme.unwrap_or_default();
                 let theme = self.theme_set.themes.get(theme.syntect_name())
                     .unwrap_or_else(|| self.theme_set.themes.iter().next().unwrap().1);
-                HighlightLines::new(syntax, theme)
+                let options = HighlightOptions {
+                    ignore_errors: true,
+                };
+                HighlightLines::new(syntax, theme, options)
             })
     }
 }
