af3425186438944d6d3537c44d07402cf20a8db0
Steven Xu <stevenxxiu@gmail.com>
2025-08-03 21:02:29 +1000

    feat: switch to Unicode placeholders
    - Image display works in *Tmux* panes.
    - Use the same code to display images inside and outside of *Tmux*.
    - `X=` and `Y=` working in *Tmux* before is more of a hack, as they exceed the offsets of the first cell.

--- src/kitty/image_renderer.rs
+++ src/kitty/image_renderer.rs
@@ -124,6 +124,51 @@ impl ImageData<'_> {
 /// according to kitty's documentation
 const CHUNK_SIZE: usize = 4096;
 
+/// Unicode placeholder character
+const PLACHOLDER: &str = "\u{10EEEE}";
+/// Unicode placeholder diacritic characters
+#[rustfmt::skip]
+const DIACRITICS: &[&str] = &[
+    "\u{0305}", "\u{030D}", "\u{030E}", "\u{0310}", "\u{0312}", "\u{033D}", "\u{033E}", "\u{033F}",
+    "\u{0346}", "\u{034A}", "\u{034B}", "\u{034C}", "\u{0350}", "\u{0351}", "\u{0352}", "\u{0357}",
+    "\u{035B}", "\u{0363}", "\u{0364}", "\u{0365}", "\u{0366}", "\u{0367}", "\u{0368}", "\u{0369}",
+    "\u{036A}", "\u{036B}", "\u{036C}", "\u{036D}", "\u{036E}", "\u{036F}", "\u{0483}", "\u{0484}",
+    "\u{0485}", "\u{0486}", "\u{0487}", "\u{0592}", "\u{0593}", "\u{0594}", "\u{0595}", "\u{0597}",
+    "\u{0598}", "\u{0599}", "\u{059C}", "\u{059D}", "\u{059E}", "\u{059F}", "\u{05A0}", "\u{05A1}",
+    "\u{05A8}", "\u{05A9}", "\u{05AB}", "\u{05AC}", "\u{05AF}", "\u{05C4}", "\u{0610}", "\u{0611}",
+    "\u{0612}", "\u{0613}", "\u{0614}", "\u{0615}", "\u{0616}", "\u{0617}", "\u{0657}", "\u{0658}",
+    "\u{0659}", "\u{065A}", "\u{065B}", "\u{065D}", "\u{065E}", "\u{06D6}", "\u{06D7}", "\u{06D8}",
+    "\u{06D9}", "\u{06DA}", "\u{06DB}", "\u{06DC}", "\u{06DF}", "\u{06E0}", "\u{06E1}", "\u{06E2}",
+    "\u{06E4}", "\u{06E7}", "\u{06E8}", "\u{06EB}", "\u{06EC}", "\u{0730}", "\u{0732}", "\u{0733}",
+    "\u{0735}", "\u{0736}", "\u{073A}", "\u{073D}", "\u{073F}", "\u{0740}", "\u{0741}", "\u{0743}",
+    "\u{0745}", "\u{0747}", "\u{0749}", "\u{074A}", "\u{07EB}", "\u{07EC}", "\u{07ED}", "\u{07EE}",
+    "\u{07EF}", "\u{07F0}", "\u{07F1}", "\u{07F3}", "\u{0816}", "\u{0817}", "\u{0818}", "\u{0819}",
+    "\u{081B}", "\u{081C}", "\u{081D}", "\u{081E}", "\u{081F}", "\u{0820}", "\u{0821}", "\u{0822}",
+    "\u{0823}", "\u{0825}", "\u{0826}", "\u{0827}", "\u{0829}", "\u{082A}", "\u{082B}", "\u{082C}",
+    "\u{082D}", "\u{0951}", "\u{0953}", "\u{0954}", "\u{0F82}", "\u{0F83}", "\u{0F86}", "\u{0F87}",
+    "\u{135D}", "\u{135E}", "\u{135F}", "\u{17DD}", "\u{193A}", "\u{1A17}", "\u{1A75}", "\u{1A76}",
+    "\u{1A77}", "\u{1A78}", "\u{1A79}", "\u{1A7A}", "\u{1A7B}", "\u{1A7C}", "\u{1B6B}", "\u{1B6D}",
+    "\u{1B6E}", "\u{1B6F}", "\u{1B70}", "\u{1B71}", "\u{1B72}", "\u{1B73}", "\u{1CD0}", "\u{1CD1}",
+    "\u{1CD2}", "\u{1CDA}", "\u{1CDB}", "\u{1CE0}", "\u{1DC0}", "\u{1DC1}", "\u{1DC3}", "\u{1DC4}",
+    "\u{1DC5}", "\u{1DC6}", "\u{1DC7}", "\u{1DC8}", "\u{1DC9}", "\u{1DCB}", "\u{1DCC}", "\u{1DD1}",
+    "\u{1DD2}", "\u{1DD3}", "\u{1DD4}", "\u{1DD5}", "\u{1DD6}", "\u{1DD7}", "\u{1DD8}", "\u{1DD9}",
+    "\u{1DDA}", "\u{1DDB}", "\u{1DDC}", "\u{1DDD}", "\u{1DDE}", "\u{1DDF}", "\u{1DE0}", "\u{1DE1}",
+    "\u{1DE2}", "\u{1DE3}", "\u{1DE4}", "\u{1DE5}", "\u{1DE6}", "\u{1DFE}", "\u{20D0}", "\u{20D1}",
+    "\u{20D4}", "\u{20D5}", "\u{20D6}", "\u{20D7}", "\u{20DB}", "\u{20DC}", "\u{20E1}", "\u{20E7}",
+    "\u{20E9}", "\u{20F0}", "\u{2CEF}", "\u{2CF0}", "\u{2CF1}", "\u{2DE0}", "\u{2DE1}", "\u{2DE2}",
+    "\u{2DE3}", "\u{2DE4}", "\u{2DE5}", "\u{2DE6}", "\u{2DE7}", "\u{2DE8}", "\u{2DE9}", "\u{2DEA}",
+    "\u{2DEB}", "\u{2DEC}", "\u{2DED}", "\u{2DEE}", "\u{2DEF}", "\u{2DF0}", "\u{2DF1}", "\u{2DF2}",
+    "\u{2DF3}", "\u{2DF4}", "\u{2DF5}", "\u{2DF6}", "\u{2DF7}", "\u{2DF8}", "\u{2DF9}", "\u{2DFA}",
+    "\u{2DFB}", "\u{2DFC}", "\u{2DFD}", "\u{2DFE}", "\u{2DFF}", "\u{A66F}", "\u{A67C}", "\u{A67D}",
+    "\u{A6F0}", "\u{A6F1}", "\u{A8E0}", "\u{A8E1}", "\u{A8E2}", "\u{A8E3}", "\u{A8E4}", "\u{A8E5}",
+    "\u{A8E6}", "\u{A8E7}", "\u{A8E8}", "\u{A8E9}", "\u{A8EA}", "\u{A8EB}", "\u{A8EC}", "\u{A8ED}",
+    "\u{A8EE}", "\u{A8EF}", "\u{A8F0}", "\u{A8F1}", "\u{AAB0}", "\u{AAB2}", "\u{AAB3}", "\u{AAB7}",
+    "\u{AAB8}", "\u{AABE}", "\u{AABF}", "\u{AAC1}", "\u{FE20}", "\u{FE21}", "\u{FE22}", "\u{FE23}",
+    "\u{FE24}", "\u{FE25}", "\u{FE26}", "\u{10A0F}", "\u{10A38}", "\u{1D185}", "\u{1D186}",
+    "\u{1D187}", "\u{1D188}", "\u{1D189}", "\u{1D1AA}", "\u{1D1AB}", "\u{1D1AC}", "\u{1D1AD}",
+    "\u{1D242}", "\u{1D243}", "\u{1D244}"
+];
+
 fn div_ceil(
     a: u32,
     b: u32,
@@ -150,8 +195,6 @@ struct KittyImage<'i> {
     data: ImageData<'i>,
     img_width: u32,
     img_height: u32,
-    cell_width: u32,
-    cell_height: u32,
     area: Area,
     is_tmux: bool,
 }
@@ -170,23 +213,27 @@ impl<'i> KittyImage<'i> {
             data,
             img_width,
             img_height,
-            cell_width: renderer.cell_width,
-            cell_height: renderer.cell_height,
             area,
             is_tmux: renderer.options.is_tmux,
         }
     }
-    /// Print X and Y escape sequence options for tmux. Inserting at the
-    /// cursor doesn't show the image at the right position.
-    fn get_tmux_xy_seq(&self) -> String {
-        if !self.is_tmux {
-            return "".into();
+    fn print_placeholder_grid(
+        &self,
+        w: &mut W,
+    ) -> Result<(), ProgramError> {
+        for y in 0..(self.area.height).min(DIACRITICS.len() as u16) {
+            w.queue(cursor::MoveTo(self.area.left, self.area.top + y))?;
+            write!(w, "\u{1b}[38;5;{}m", self.id)?;
+            for x in 0..(self.area.width).min(DIACRITICS.len() as u16) {
+                write!(
+                    w,
+                    "{}{}{}",
+                    PLACHOLDER, DIACRITICS[y as usize], DIACRITICS[x as usize]
+                )?;
+            }
+            write!(w, "\u{1b}[39m")?;
         }
-        format!(
-            ",X={},Y={}",
-            self.area.left * self.cell_width as u16,
-            self.area.top * self.cell_height as u16,
-        )
+        Ok(())
     }
     /// Render the image by sending multiple kitty escape sequences, each
     /// one with part of the image raw data (encoded as base64)
@@ -196,9 +243,6 @@ impl<'i> KittyImage<'i> {
     ) -> Result<(), ProgramError> {
         let esc = get_esc_seq(self.is_tmux);
         let encoded = BASE64.encode(self.data.bytes());
-        if !self.is_tmux {
-            w.queue(cursor::MoveTo(self.area.left, self.area.top))?;
-        }
         let mut pos = 0;
         loop {
             if self.is_tmux {
@@ -207,7 +251,7 @@ impl<'i> KittyImage<'i> {
             if pos + CHUNK_SIZE < encoded.len() {
                 write!(
                     w,
-                    "{}_Ga=t,f={},t=d,i={},s={},v={},m=1;{}{}\\",
+                    "{}_Gq=2,a=t,f={},t=d,i={},s={},v={},m=1;{}{}\\",
                     &esc,
                     self.data.kitty_format(),
                     self.id,
@@ -224,7 +268,7 @@ impl<'i> KittyImage<'i> {
                 // last chunk
                 write!(
                     w,
-                    "{}_Gm=0;{}{}\\",
+                    "{}_Gq=2,m=0;{}{}\\",
                     &esc,
                     &encoded[pos..encoded.len()],
                     &esc
@@ -238,17 +282,13 @@ impl<'i> KittyImage<'i> {
                 }
                 write!(
                     w,
-                    "{}_Ga=p,i={},c={},r={}{}{}\\",
-                    &esc,
-                    self.id,
-                    self.area.width,
-                    self.area.height,
-                    &self.get_tmux_xy_seq(),
-                    &esc,
+                    "{}_Gq=2,a=p,U=1,i={},c={},r={}{}\\",
+                    &esc, self.id, self.area.width, self.area.height, &esc,
                 )?;
                 if self.is_tmux {
                     tmux_write_tail!(w)?
                 }
+                self.print_placeholder_grid(w)?;
                 break;
             }
         }
@@ -274,40 +314,26 @@ impl<'i> KittyImage<'i> {
             .ok_or_else(|| io::Error::other("Path can't be converted to UTF8"))?;
         let encoded_path = BASE64.encode(path);
         debug!("temp file written: {:?}", path);
-        w.queue(cursor::MoveTo(self.area.left, self.area.top))?;
         if self.is_tmux {
             tmux_write_header!(w)?;
         }
         write!(
             w,
-            "{}_Ga=t,f={},t=t,i={},s={},v={};{}{}\\",
+            "{}_Gq=2,a=T,U=1,f={},t=t,i={},s={},v={},c={},r={};{}{}\\",
             &esc,
             self.data.kitty_format(),
             self.id,
             self.img_width,
             self.img_height,
+            self.area.width,
+            self.area.height,
             encoded_path,
             &esc,
         )?;
         if self.is_tmux {
             tmux_write_tail!(w)?;
         }
-        if self.is_tmux {
-            tmux_write_header!(w)?;
-        }
-        write!(
-            w,
-            "{}_Ga=p,i={},c={},r={}{}{}\\",
-            &esc,
-            self.id,
-            self.area.width,
-            self.area.height,
-            self.get_tmux_xy_seq(),
-            &esc,
-        )?;
-        if self.is_tmux {
-            tmux_write_tail!(w)?;
-        }
+        self.print_placeholder_grid(w)?;
         Ok(())
     }
 }
