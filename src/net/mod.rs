f6b16c3b9d0f00af95ed818a80da49435de72520
Denys Séguret <cano.petrole@gmail.com>
2025-09-12 18:49:59 +0200

    fix socket not writable on termux (#1046)
    Fix #1045

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -9,5 +9,15 @@ pub use {
 };
 
 pub fn socket_file_path(server_name: &str) -> String {
+    #[cfg(target_os = "android")]
+    {
+        // On termux, /tmp is not writable and we're supposed
+        // to use /data/data/com.termux/files/usr/tmp
+        let usr_dir = "/data/data/com.termux/files/usr";
+        if std::path::Path::new(usr_dir).is_dir() {
+            return format!("{}/tmp/broot-server-{}.sock", usr_dir, server_name);
+        }
+        // maybe we're not in termux ? Fallback to /tmp
+    }
     format!("/tmp/broot-server-{server_name}.sock")
 }
