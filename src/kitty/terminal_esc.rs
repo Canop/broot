0eb91c97c055934ee0c1bd3ad6aa08716938a0ce
Steven Xu <stevenxxiu@users.noreply.github.com>
2025-10-31 17:45:05 +1100

    fix: remove extra `\e` (#1091)

--- src/kitty/terminal_esc.rs
+++ src/kitty/terminal_esc.rs
@@ -6,7 +6,7 @@ pub fn get_tmux_header(tmux_nest_count: u32) -> String {
     let mut header: String = "".into();
     for i in 0..tmux_nest_count {
         header.push_str(&"\u{1b}".repeat(2usize.pow(i)));
-        header.push_str("\u{1b}Ptmux;");
+        header.push_str("Ptmux;");
     }
     header
 }
