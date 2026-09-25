b95c399ecee922058ec5235d69589a12bc59e642
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-09-25 16:19:10 -0400

    Use e6b2 icon for nerd font TOML file types

--- resources/icons/nerdfont/data/icon_name_to_icon_code_point_map.rs
+++ resources/icons/nerdfont/data/icon_name_to_icon_code_point_map.rs
@@ -381,7 +381,7 @@
 ( "file_type_light_testjs", 0xf0668 ), // 󰙨
 ( "file_type_light_tex", 0xe69b ), // 
 ( "file_type_light_todo", 0xe69c ), // 
-( "file_type_light_toml", 0xf0626 ), // 󰘦
+( "file_type_light_toml", 0xe6b2 ), // 
 ( "file_type_light_unibeautify", 0xf15b ),
 ( "file_type_light_vash", 0xf15b ),
 ( "file_type_light_vsix", 0xf15b ),
@@ -709,7 +709,7 @@
 ( "file_type_textile", 0xf15b ),
 ( "file_type_tfs", 0xf15b ),
 ( "file_type_todo", 0xe69c ), // 
-( "file_type_toml", 0xf0626 ), // 󰘦
+( "file_type_toml", 0xe6b2 ), // 
 ( "file_type_tox", 0xf15b ),
 ( "file_type_travis", 0xe77e ), // 
 ( "file_type_tsconfig", 0xe628 ), // 
